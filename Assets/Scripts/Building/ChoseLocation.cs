f12b543cbd20bfc85ee9f69781a058fc4936e5c7
Ilya Tsykunov (tsykunoi) <tsykunoi@coventry.ac.uk>
2018-12-01 10:29:07 +0000

    NavMesh

--- Assets/Scripts/Building/ChoseLocation.cs
+++ Assets/Scripts/Building/ChoseLocation.cs
@@ -5,43 +5,60 @@ using UnityEngine;
 public class ChoseLocation : MonoBehaviour {
 
     [SerializeField]
-    protected bool isSelected;
+    protected bool isBeingMoved;
+    [SerializeField]
+    protected bool canBePlaced;
+
     [SerializeField]
     private Vector3 mousePosition;
     [SerializeField]
     private float distanceToGround;
+    private float yAxis;
 
     // Use this for initialization
     void Start () {
-        
-        isSelected = true;
-	}
+
+        yAxis = 2f;
+        isBeingMoved = true;
+        canBePlaced = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        FollowMouse();
+        if(isBeingMoved == true)
+        {
+            FollowMouse();
+            PlaceBuilding();
+        }
+    }
 
+    void PlaceBuilding()
+    {
+        if (Input.GetMouseButtonDown(0) && canBePlaced == true)
+        {
+            gameObject.transform.position = new Vector3(mousePosition.x, 1f, mousePosition.z);
+            isBeingMoved = false;
+        }
     }
 
     void FollowMouse()
     {
-        mousePosition = Camera.main.GetComponent<MousePos>().mousePosition;
+        RaycastHit rayHit;
 
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity))
+        {
+            mousePosition = rayHit.point;
+        }
         GetDistanceToGround();
-        if(distanceToGround > 1f && distanceToGround < 1.5f)
+        /*if (distanceToGround > 2f)
         {
-            transform.position = new Vector3(mousePosition.x, transform.position.y - 0.1f, mousePosition.y);
+            yAxis -= 0.01f;
         }
-        else if (distanceToGround < 1f && distanceToGround > 0.5f)
+        else if(distanceToGround < 2f)
         {
-            transform.position = new Vector3(mousePosition.x, transform.position.y + 0.1f, mousePosition.y);
-        }
-        else
-        {
-            transform.position = new Vector3(mousePosition.x, transform.position.y, mousePosition.y);
-        }
-        
-
+            yAxis += 0.01f;
+        }*/
+        transform.position = new Vector3(mousePosition.x, yAxis, mousePosition.z);
     }
     void GetDistanceToGround()
     {
@@ -52,6 +69,20 @@ public class ChoseLocation : MonoBehaviour {
             {
                 distanceToGround = rayHit.distance;
             }
+        }
+    }
+    void OnCollisionStay(Collision collider)
+    {
+        if (collider.gameObject.tag == "Building")
+        {
+            canBePlaced = false;
+        }
+    }
+    void OnCollisionExit(Collision collider)
+    {
+        if (collider.gameObject.tag == "Building")
+        {
+            canBePlaced = true;
         }
     }
 }
---
+++ Assets/Scripts/World/Navigation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Navigation : MonoBehaviour {
+
+
+    private void Start()
+    {
+        
+    }
+
+    void BakeMap()
+    {
+        nav
+    }
+}
